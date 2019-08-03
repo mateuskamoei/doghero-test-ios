2c7ff920084fec47e59a2d7b037603ea44fbcead
mateuskamoei <mateuskamoei@gmail.com>
2019-08-03 19:42:10 -0300

    First commit

---
+++ doghero-test/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  doghero-test
+//
+//  Created by Gustavo Romito Nogueira on 1/27/18.
+//  Copyright © 2018 doghero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
